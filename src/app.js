a117fddfa370396c559f87b2dc63ed23f93c7917
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2024-10-07 16:26:37 +0000

    html finalizado (beta)

--- src/app.js
+++ src/app.js
@@ -19,29 +19,31 @@ window.onload = function() {
   };
 
   const FiltrarArrays = (arr, domain) => {
+    // funcion para filtrar los dominios
     domain.forEach(element => {
+      //filtramos por elemento
       for (let i = 0; i < arr.length; i++) {
+        //comprobamos las dos ultimas silabas
         if (
-          arr[i].slice(arr[i].length - 3, arr[i].length).toLowerCase() ==
+          arr[i].slice(arr[i].length - 3, arr[i].length).toLowerCase() == //comprobamos las tres ultimas silabas
           element
         ) {
-          arr[i] = `${arr[i].slice(0, arr[i].length - 3)}.${element}`;
+          arr[i] = `${arr[i].slice(0, arr[i].length - 3)}.${element}`; //actualizamos el valor agregando el dominio
         } else if (
-          arr[i].slice(arr[i].length - 2, arr[i].length).toLowerCase() ==
+          arr[i].slice(arr[i].length - 2, arr[i].length).toLowerCase() == //comprobamos las dos ultimas silabas
           element
         ) {
-          arr[i] = `${arr[i].slice(0, arr[i].length - 2)}.${element}`;
+          arr[i] = `${arr[i].slice(0, arr[i].length - 2)}.${element}`; //actualizamos el valor agregando el dominio
         }
       }
     });
     return arr;
   };
 
-  let pronoun = ["the", "our", "this", "that", "my"], // dominios
-    adj = ["great", "big", "ejemplus", "little", "amazing", "crazy", "funny"],
+  //Dominios
+  let pronoun = ["the", "our", "this", "that", "my"],
+    adj = ["great", "big", "little", "amazing", "crazy", "funny"],
     noun = [
-      "jogger",
-      "racoon",
       "telecom",
       "intercom",
       "dotcom",
@@ -54,7 +56,27 @@ window.onload = function() {
     ],
     domain = ["com", "net", "us", "io"];
 
-  let ar = CompararArrays(pronoun, adj),
-    arr = CompararArrays(ar, noun);
-  console.log(FiltrarArrays(arr, domain));
+  let arr = CompararArrays(pronoun, adj);
+
+  arr = CompararArrays(arr, noun);
+  arr = FiltrarArrays(arr, domain);
+
+  console.log(arr);
+
+  const boton = document.getElementById("ejemplo"),
+    domainhtml = document.getElementById("domain");
+
+  for (let i = 0; i < arr.length; i++) {
+    domainhtml.innerHTML += `
+      <tr>
+          <th scope="row">${i + 1}</th>
+          <td>${arr[i]}</td>
+          <td>${arr[i].toUpperCase()}</td>
+          <td><p>.com</p></td>
+      </tr>`;
+  }
+
+  boton.addEventListener("click", function() {
+    domainhtml.innerHTML = "";
+  });
 };
