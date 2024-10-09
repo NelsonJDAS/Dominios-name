d4b20774e7090626c054a5960056b0232d7cfbd3
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2024-10-09 18:01:15 +0000

    Last_Version

--- src/app.js
+++ src/app.js
@@ -6,33 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //Domains
-  let domains = [
-    "com",
-    "org",
-    "net",
-    "edu",
-    "gov",
-    "app",
-    "biz",
-    "co",
-    "xyz",
-    "me",
-    "io"
-  ];
+  const ELEMENTOSTABLA = () => {
+    for (let i = 0; i < 6; i++) {
+      contenedorDomains.innerHTML += `
+      <tr>
+        <th scope="row">x</th>
+        <td>--------------</td>
+        <td>--------------</td>
+        <td>--------------</p></td>
+      </tr>`;
+    }
+  };
 
-  //variables interactivas junto al html
-  const btnlimpiartabla = document.getElementById("btnLimpiar"),
-    btnSubirDomain = document.getElementById("btnSubirDomain"),
-    inputsubirdomain = document.getElementById("inputdomain"),
-    btnMostrarTodos = document.getElementById("all"),
-    botonesFiltrar = document.querySelectorAll(".btnfiltrar"),
-    contenedorDomains = document.getElementById("containerDomains");
-
-  let arrDomains = [],
+  const MOSTRARTODOSTABLA = () => {
     numberId = 1;
-
-  btnMostrarTodos.addEventListener("click", () => {
     contenedorDomains.innerHTML = "";
     arrDomains.forEach(arr => {
       for (let i = 0; i < domains.length; i++) {
@@ -46,11 +33,52 @@ window.onload = function() {
         numberId = numberId + 1;
       }
     });
+  };
+
+  //Domains
+  let domains = [
+      "com",
+      "org",
+      "net",
+      "edu",
+      "gov",
+      "app",
+      "biz",
+      "co",
+      "xyz",
+      "me",
+      "io"
+    ],
+    //varianbles interactivas junto al html
+    arrDomains = [],
+    numberId = 1;
+
+  //constantes de referencias del html
+  const btnlimpiartabla = document.getElementById("btnLimpiar"),
+    btnSubirDomain = document.getElementById("btnSubirDomain"),
+    inputsubirdomain = document.getElementById("inputdomain"),
+    btnMostrarTodos = document.getElementById("all"),
+    botonesFiltrar = document.querySelectorAll(".btnfiltrar"),
+    contenedorDomains = document.getElementById("containerDomains");
+
+  //Eventos
+
+  btnlimpiartabla.addEventListener("click", () => {
+    arrDomains = [];
+    contenedorDomains.innerHTML = `
+    `;
+    ELEMENTOSTABLA();
   });
+
   btnSubirDomain.addEventListener("click", () => {
     inputsubirdomain.placeholder = "Domain Added!";
     arrDomains.push(inputsubirdomain.value);
     inputsubirdomain.value = "";
+    MOSTRARTODOSTABLA();
+  });
+
+  btnMostrarTodos.addEventListener("click", () => {
+    MOSTRARTODOSTABLA();
   });
 
   botonesFiltrar.forEach(btn => {
